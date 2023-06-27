7e0e9e0283f2fc19f016ac29934037e4fe1815ea
koryruno <koryruno@yandex.ru>
2023-06-27 15:32:57 +0300

    add tests, not confirmed yet

--- src/main/java/ru.yandex.practicum.kanban/Main.java
+++ src/main/java/ru.yandex.practicum.kanban/Main.java
@@ -1,11 +1,44 @@
 package ru.yandex.practicum.kanban;
 
+import ru.yandex.practicum.kanban.models.Epic;
+import ru.yandex.practicum.kanban.models.Subtask;
+import ru.yandex.practicum.kanban.models.Task;
+import ru.yandex.practicum.kanban.services.TaskManager;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Поехали!");
+        System.out.println("Program is not ready yet! Please wait...");
 
-        /*Decompoze:
-        Class*/
+        // Test
+        TaskManager taskManager = new TaskManager();
+        Task task1 = new Task("Task1", "Taskdes1");
+        Task task2 = new Task("Task2", "Taskdes2");
+        taskManager.addTask(task1);
+        taskManager.addTask(task2);
 
+        Epic epic1 = new Epic("Epic1", "Epicdes1");
+        Epic epic2 = new Epic("Epic2", "Epicdes2");
+        Epic epic3 = new Epic("Epic3", "Epicdes3");
+        taskManager.addEpic(epic1);
+        taskManager.addEpic(epic2);
+        taskManager.addEpic(epic3);
+
+        Subtask subtask1 = new Subtask(1,"Sub1","Subdes1");
+        Subtask subtask2 = new Subtask(1,"Sub2","Subdes2");
+        Subtask subtask3 = new Subtask(2,"Sub1","Subdes1");
+        taskManager.addSubTask(subtask1);
+        taskManager.addSubTask(subtask2);
+        taskManager.addSubTask(subtask3);
+
+        System.out.println(taskManager.getTask(1));
+        System.out.println(taskManager.getTask(2));
+
+        System.out.println(taskManager.getEpic(1));
+        System.out.println(taskManager.getEpic(2));
+        System.out.println(taskManager.getEpic(3));
+
+        System.out.println(taskManager.getSubTask(1));
+        System.out.println(taskManager.getSubTask(2));
+        System.out.println(taskManager.getSubTask(3));
     }
 }
