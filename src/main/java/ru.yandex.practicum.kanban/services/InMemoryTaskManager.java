907bb764b133439def31e75d6ec36cf3de09f1c7
koryruno <koryruno@yandex.ru>
2023-07-07 17:44:19 +0300

    ver 2.0

--- src/main/java/ru.yandex.practicum.kanban/services/InMemoryTaskManager.java
+++ src/main/java/ru.yandex.practicum.kanban/services/InMemoryTaskManager.java
@@ -21,7 +21,7 @@ public class InMemoryTaskManager implements TaskManager {
         taskStorage = new HashMap<>();
         epicStorage = new HashMap<>();
         subTaskStorage = new HashMap<>();
-        historyManager = new InMemoryHistoryManager();
+        historyManager = Manager.getDefaultHistory();
     }
 
 
