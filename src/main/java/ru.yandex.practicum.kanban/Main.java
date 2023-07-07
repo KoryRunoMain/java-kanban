c7e743154b7e5404b9c046ecc948bcf7ecaeb7fb
koryruno <koryruno@yandex.ru>
2023-07-07 19:06:40 +0300

    DONE

--- src/main/java/ru.yandex.practicum.kanban/Main.java
+++ src/main/java/ru.yandex.practicum.kanban/Main.java
@@ -1,10 +1,10 @@
 package ru.yandex.practicum.kanban;
 
+import ru.yandex.practicum.kanban.interfaces.TaskManager;
 import ru.yandex.practicum.kanban.models.Epic;
-import ru.yandex.practicum.kanban.models.Status;
 import ru.yandex.practicum.kanban.models.Subtask;
 import ru.yandex.practicum.kanban.models.Task;
-import ru.yandex.practicum.kanban.services.TaskManager;
+import ru.yandex.practicum.kanban.services.Manager;
 
 public class Main {
     public static void main(String[] args) {
