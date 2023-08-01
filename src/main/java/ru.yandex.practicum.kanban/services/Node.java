c2cf3173416b3937122d504abd926bf3bc4fff96
koryruno <koryruno@yandex.ru>
2023-08-01 13:19:46 +0300

    UPDATE! Sprint 5

---
+++ src/main/java/ru.yandex.practicum.kanban/services/Node.java
@@ -0,0 +1,37 @@
+package ru.yandex.practicum.kanban.services;
+
+public class Node<T> {
+    private T value;
+    private Node<T> prev;
+    private Node<T> next;
+
+    public Node(T value, Node<T> prev, Node<T> next) {
+        this.value = value;
+        this.prev = prev;
+        this.next = next;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public void setPrev(Node<T> prev) {
+        this.prev = prev;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public Node<T> getPrev() {
+        return prev;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+}
