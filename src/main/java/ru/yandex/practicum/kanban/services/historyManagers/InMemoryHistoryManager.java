4ff6d5345fe6288bb34d3e4556846498e30884e3
koryruno <koryruno@yandex.ru>
2023-08-02 16:59:34 +0300

    Sprint_5

--- src/main/java/ru/yandex/practicum/kanban/services/historyManagers/InMemoryHistoryManager.java
+++ src/main/java/ru/yandex/practicum/kanban/services/historyManagers/InMemoryHistoryManager.java
@@ -2,7 +2,10 @@ package ru.yandex.practicum.kanban.services.historyManagers;
 
 import ru.yandex.practicum.kanban.models.Task;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
