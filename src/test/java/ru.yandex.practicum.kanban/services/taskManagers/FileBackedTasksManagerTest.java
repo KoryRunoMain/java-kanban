17a513a7867ca5f008e5efd64579b2c643b61f0d
koryruno <koryruno@yandex.ru>
2023-12-21 16:56:07 +0300

    Re-work some

---
+++ src/test/java/ru.yandex.practicum.kanban/services/taskManagers/FileBackedTasksManagerTest.java
@@ -0,0 +1,93 @@
+package ru.yandex.practicum.kanban.services.taskManagers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.kanban.models.Epic;
+import ru.yandex.practicum.kanban.models.Subtask;
+import ru.yandex.practicum.kanban.models.Task;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileBackedTasksManagerTest extends TaskManagerTest<FileBackedTasksManager> {
+
+    protected File file;
+
+    // Отчистка коллекций задач для тестов
+    protected void deleteAllTasksForTest() {
+        taskManager.removeAllTasks();
+        taskManager.removeAllEpics();
+        taskManager.removeAllSubTasks();
+    }
+
+    // Загрузка истории задач для тестов
+    protected void createTasksHistory() {
+        taskManager.historyManager.add(task);
+        taskManager.historyManager.add(epic);
+        taskManager.historyManager.add(subtask);
+    }
+
+    // Удаление истории задач для тестов
+//    protected void removeTasksHistory() {
+//        taskManager.historyManager.remove(task);
+//        taskManager.historyManager.remove(epic);
+//        taskManager.historyManager.remove(subtask);
+//    }
+
+
+    @BeforeEach
+    public void initFile() {
+        file = new File("src/resources/" + "tasksTest" + ".csv");
+    }
+
+    @Test
+    public void checkLoadFromFile() {
+        FileBackedTasksManager taskMan = FileBackedTasksManager.loadFromFile(file);
+        final List<Task> tasks = taskMan.getAllTasks();
+        assertNotNull(tasks, "Список задач пуст.");
+        assertEquals(1, tasks.size(), "Список задач пуст.");
+    }
+
+    // Пустой список задач
+    @Test
+    public void checkLoadFromFileWithNoTasks() {
+        deleteAllTasksForTest();
+        FileBackedTasksManager taskMan = FileBackedTasksManager.loadFromFile(file);
+        final List<Task> tasks = taskMan.getAllTasks();
+        final List<Epic> epics = taskMan.getAllEpics();
+        final List<Subtask> subtasks = taskMan.getAllSubTasks();
+        assertNotNull(tasks, "Список задач не пустой.");
+        assertEquals(0, tasks.size(), "Список задач не пустой.");
+        assertNotNull(epics, "Список задач пуст.");
+        assertEquals(0, epics.size(), "Список задач (Epic) не пустой.");
+        assertNotNull(subtasks, "Список задач пуст.");
+        assertEquals(0, subtasks.size(), "Список подзадач не пустой.");
+    }
+
+    //Эпик без подзадач
+    @Test
+    public void checkLoadFromEpicFileWithoutSubtasks() {
+        deleteAllTasksForTest();
+        assertEquals(0, taskManager.getAllSubTasks().size(), "Список подзадач не пустой.");
+        FileBackedTasksManager taskMan = FileBackedTasksManager.loadFromFile(file);
+        assertEquals(1, taskMan.getAllEpics().size(), "Список задач (Epic) не пустой.");
+        assertEquals(0, taskMan.getAllSubTasks().size(), "Список подзадач не пустой.");
+    }
+
+    //Пустой список истории
+    @Test
+    public void checkLoadFromEmptyFileOfHistory() {
+        createTasksHistory();
+        FileBackedTasksManager taskMan = FileBackedTasksManager.loadFromFile(file);
+        assertEquals(3, taskManager.historyManager.getHistory().size(), "Файл с историрей пустой.");
+
+
+    }
+
+
+
+}
