ea51e03c37769dba9f4ea62db1c034627e78754f
koryruno <koryruno@yandex.ru>
2024-01-14 19:24:37 +0300

    ADD:Check_Code_(Sprint#8)_v1.0

--- src/main/java/ru.yandex.practicum.kanban/services/Managers.java
+++ src/main/java/ru.yandex.practicum.kanban/services/Managers.java
@@ -14,9 +14,9 @@ import java.time.Instant;
 
 public class Managers {
 
-    public static TaskManager getDefault() {
-        return new InMemoryTaskManager(getDefaultHistory());
-    }
+//    public static TaskManager getDefault() {
+//        return new InMemoryTaskManager(getDefaultHistory());
+//    }
 
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
