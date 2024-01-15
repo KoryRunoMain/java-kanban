4d84f620dc9f6212705a0bb15039e8c5b4cc84ad
koryruno <koryruno@yandex.ru>
2024-01-16 00:43:35 +0300

    (Sprint#8_v1.0)

--- src/main/java/ru.yandex.practicum.kanban/services/taskManagers/TaskManager.java
+++ src/main/java/ru.yandex.practicum.kanban/services/taskManagers/TaskManager.java
@@ -10,31 +10,31 @@ import java.util.List;
 
 public interface TaskManager {
 
-    /*TASK*/
+    //TASK
     Task createTask(Task task); //TASK.Создать
     Task getTaskById(int id); // TASK.Получить
     void removeTaskById(int id); // TASK.Удалить
     void updateTask(Task updateTask); // TASK.Обновить
     void removeAllTasks(); // TASK.Удалить все задачи
+    ArrayList<Task> getAllTasks(); // TASK.Получить все задачи
 
-    /*EPIC*/
+    //EPIC
     Epic createEpic(Epic epic); // EPIC.Создать
     Epic getEpicById(int id); // EPIC.Получить
     void removeEpicById(int id); // EPIC.Удалить
     void updateEpic(Epic updateEpic); // EPIC.Обновить
     void removeAllEpics(); // EPIC.Удалить все задачи
+    ArrayList<Epic> getAllEpics(); // EPIC.Получить все задачи
 
-    /*SUBTASK*/
+    //SUBTASK
     Subtask createSubTask(Subtask subtask); // SUBTASK.Создать
     Subtask getSubTaskById(int id); // SUBTASK.Получить
     void removeSubTaskById(int id); // SUBTASK.Удалить
     void updateSubTask(Subtask updateSubtask); // SUBTASK.Обновить
     void removeAllSubTasks(); // SUBTASK.Удалить все подзадачи
-
-    /*Другие методы*/
-    ArrayList<Task> getAllTasks(); // TASK.Получить все задачи
-    ArrayList<Epic> getAllEpics(); // EPIC.Получить все задачи
     ArrayList<Subtask> getAllSubTasks(); // SUBTASK.Получить все задачи
+
+    //Другие методы
     List<Task> getPrioritizedTasks(); // Получить список приоритетных задач
     List<Task> getHistory(); // Получить историю задач
     List<Subtask> getSubTasksOfEpic(Epic epic); // Получить список SubTask всех подзадач EPIC
