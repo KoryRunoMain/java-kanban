d9bc86969b90c1f52b095962801e9222fddbb09c
koryruno <koryruno@yandex.ru>
2024-01-16 21:47:59 +0300

    FIX:delete extends of KVServer(Sprint#8_v1.0)

--- src/main/java/ru.yandex.practicum.kanban/httpServer/HttpTaskServer.java
+++ src/main/java/ru.yandex.practicum.kanban/httpServer/HttpTaskServer.java
@@ -8,19 +8,21 @@ import ru.yandex.practicum.kanban.httpServer.handlers.PrioritizedTasksHandler;
 import ru.yandex.practicum.kanban.httpServer.handlers.SubtaskHandler;
 import ru.yandex.practicum.kanban.httpServer.handlers.SubtasksOfEpicHandler;
 import ru.yandex.practicum.kanban.httpServer.handlers.TaskHandler;
+import ru.yandex.practicum.kanban.models.Task;
 import ru.yandex.practicum.kanban.services.Managers;
 import ru.yandex.practicum.kanban.services.taskManagers.TaskManager;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
-public class HttpTaskServer extends KVServer {
+public class HttpTaskServer {
     private final HttpServer server;
+    private final TaskManager taskManager;
     private final Gson gson;
     public static final int PORT = 8080;
 
     public HttpTaskServer() throws IOException {
-        TaskManager taskManager = Managers.getDefault();
+        taskManager = Managers.getDefault();
         gson = Managers.getGson();
         server = HttpServer.create(new InetSocketAddress(PORT), 0);
         // endPoints
@@ -29,13 +31,13 @@ public class HttpTaskServer extends KVServer {
         server.createContext("/tasks/subtask", new SubtaskHandler(taskManager));
         server.createContext("/tasks", new PrioritizedTasksHandler(taskManager));
         server.createContext("/tasks/history", new HistoryHandler(taskManager));
-        server.createContext("/tasks/subtask/epic/", new SubtasksOfEpicHandler(taskManager));
+        server.createContext("/tasks/subtask/epic", new SubtasksOfEpicHandler(taskManager));
     }
 
     public void start() {
+        server.start();
         System.out.println("Started TaskServer " + PORT);
         System.out.println("http://localhost:" + PORT + "/api/v1/tasks");
-        server.start();
     }
 
     public void stop() {
